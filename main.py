2042d79a102dd2024697c704f1dcbdb1795f1d9c
mikhio <mikhail.iogansen@gmail.com>
2020-02-17 22:53:38 +0300

    Add grounds and get colors

--- main.py
+++ main.py
@@ -2,28 +2,47 @@ import tkinter as tk
 
 
 def init():
-	global window, root, my_ball
+	global window, root, window_width, window_height
 
 	root = tk.Tk()
 
 	window_width = 600
 	window_height = 500
-	window_color = "#eeeeee"
+	window_color = "#694a0d"
 
 	window = tk.Canvas(root, bg = window_color, width = window_width, height = window_height)
 	window.pack()
 
-	ball_width = 75
-	ball_x1 = 50
-	ball_y1 = window_height - 50 - ball_width 
 
-	my_ball = Ball(ball_x1, ball_y1, ball_width, "my_ball", fill_color = "red")
+	game()
 
-	root.bind('<Key>', handler_move)
+
+class Floor:
+	def __init__(self, x1 ,y1, width, height, tags, fill_color = "#828282", outline_color = None):
+		self.limit_points = [x1, y1, x1 + width, y1 + height]
+		self.fill_color = fill_color
+		self.tags = tags
+
+		if outline_color == None:
+			self.outline_color = fill_color
+		else:
+			self.outline_color = outline_color
+
+		self.draw()
+
+
+
+	def draw(self):
+		window.create_rectangle(
+			self.limit_points,
+			tags = self.tags,
+			fill = self.fill_color,
+			outline = self.outline_color
+		)
 
 
 class Ball:
-	def __init__(self, x1, y1, width, tags, fill_color = "black", outline_color = None):
+	def __init__(self, x1, y1, width, tags = None, fill_color = "black", outline_color = None):
 		self.limit_points = [x1, y1, x1 + width, y1 + width]
 		self.fill_color = fill_color
 		self.tags = tags
@@ -54,19 +73,38 @@ class Ball:
 			self.limit_points[0] += speed
 			self.limit_points[2] += speed
 
+	def jump(self):
+		pass
+
 
 def handler_move(event):
-	window.delete(my_ball.tags)
+	window.delete(player.tags)
 	speed = 5
 
 	if event.char == "d":
-		my_ball.move("right", speed)
-		my_ball.draw()
+		player.move("right", speed)
+		player.draw()
 	elif event.char == "a":
-		my_ball.move("left", speed)
-		my_ball.draw()
+		player.move("left", speed)
+		player.draw()
 	else:
-		my_ball.draw()
+		player.draw()
+
+
+def game():
+	global player
+
+	main_floor = Floor(0, 450, window_width, 50, tags = "main_floor",)
+	left_ground = Floor(50, 250, 200, 50, tags = "left_ground",)
+	right_ground = Floor(350, 250, 200, 50, tags = "right_ground",)
+
+
+	player_width = 75
+	player_x1 = 50
+	player_y1 = window_height - 52 - player_width
+
+	player = Ball(player_x1, player_y1, player_width, "player", fill_color = "#d6c52b")
+	root.bind('<Key>', handler_move)
 
 
 
